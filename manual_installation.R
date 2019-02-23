505dac2f70a4ae002f1b73f73872fdfa7d6f4ad5
ebinabo <45301854+ebinabo@users.noreply.github.com>
2019-02-23 20:13:48 +0300

    Update manual_installation.R

--- manual_installation.R
+++ manual_installation.R
@@ -5,4 +5,6 @@ install.packages("purrr")
 
 library(purrr)
 
-c("magrittr", "httr", "stringr") %>% map(install.packages)
+c("magrittr", "httr", "stringr", "devtools", "roxygen2") %>% map(install.packages)
+
+# After sourcing the script, Go to Build tab -> Configure Build tools ... -> tick Generate documentation with roxygen
